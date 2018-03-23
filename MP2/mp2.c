87703c4e2b435347bf9de4b32d7d9dc392fc2747
bzhang70 <bzhang70@illinois.edu>
2018-03-23 14:57:51 -0500

    mp2

--- MP2/mp2.c
+++ MP2/mp2.c
@@ -18,9 +18,15 @@ MODULE_DESCRIPTION("CS-423 MP2");
 #define FILE        "status"
 #define _MAX_SIZE   1024
 #define T_INTERVAL  5000
+#define SLEEPING    0
+#define READY       1
+#define RUNNING    2
 
+DEFINE_MUTEX(task_mutex);
 static spinlock_t mp2_lock;
 static struct kmem_cache *mp2_cache;
+static struct task_struct *dispatcher;
+LIST_HEAD(task_list);
 
 typedef struct {
     struct task_struct *linux_task;
@@ -32,6 +38,8 @@ typedef struct {
     unsigned int state;
     unsigned long next_period;
 } mp2_task_struct;
+
+mp2_task_struct *current_mp2_task = NULL;
 
 
 static const struct file_operations mp2_fops = {
@@ -45,11 +53,10 @@ static const struct file_operations mp2_fops = {
 static ssize_t mp2_read(struct file *file, char __user *buffer, size_t count, loff_t *data){
     ssize_t copied = 0;
     char *buf = (char *)kmalloc(count, GFP_KERNEL);
-    mp2_task_struct *tmp;
-	struct list_head *pos;
+    mp2_task_struct *tmp, *n;
     unsigned long flags;
-    spin_lock_irqsave(&mp2_lock,flags);
-    list_for_each_entry(tmp, &task_list, list){
+	spin_lock_irqsave(&mp2_lock,flags);
+    list_for_each_entry_safe(tmp, n, &task_list, list){
 		copied += sprintf(buf+copied,"%u %u %u %u\n",tmp->pid,tmp->period,tmp->cpu_time, tmp->state);
 	}
     spin_unlock_irqrestore(&mp2_lock,flags);
@@ -132,12 +139,10 @@ void registration(unsigned int pid,unsigned long period, unsigned long cpu_time)
 
 
 void de_registration(unsigned int pid){
-	struct list_head *pos, *n;
-	mp2_task_struct *tmp;
+	mp2_task_struct *tmp, *n;
 	unsigned long flags;
 	spin_lock_irqsave(&mp2_lock,flags);
-	list_for_each_safe(pos,n,&task_list){
-		tmp = list_entry(pos,struct mp2_task_struct,list);
+	list_for_each_entry_safe(tmp,n,&task_list, list){
 		if(tmp->pid == pid){
 			list_del(&tmp->list);
 			del_timer(&tmp->wakeup_timer);
@@ -153,6 +158,116 @@ void de_registration(unsigned int pid){
 	}
 	spin_unlock_irqrestore(&mp2_lock,flags);
 	printk("task %u de-registered!",pid);
+}
+
+int dispatch_thread(void *data){
+	while(1) {
+		mp2_task_struct *sel;
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+		if (kthread_should_stop()) {
+            return 0;
+        }
+		mutex_lock_interruptible(&task_mutex);
+
+        // select the one with highest priority
+        mp2_task_struct *tmp, *n;
+    	unsigned long flags;
+    	spin_lock_irqsave(&mp2_lock,flags);
+    	list_for_each_entry_safe(tmp,n,&task_list, list){
+            if(tmp->period < invPrior && tmp->state == READY){
+    			sel = tmp;
+    			invPrior = tmp->period;
+    		}
+    	}
+    	spin_unlock_irqrestore(&mp2_lock,flags);
+
+		if(sel == NULL) {
+			if(current_mp2_task){
+				current_mp2_task->state = READY;
+				set_scheduler(current_mp2_task->linux_task, SCHED_NORMAL, 0);
+			}
+		}
+		else if((current_mp2_task && current_mp2_task->period > sel->period) || !current_mp2_task){
+			if(current_mp2_task) {
+				current_mp2_task->state = READY;
+				//for the old running task (preempted task), the dispatching thread should execute the following code:
+				set_scheduler(current_mp2_task->linux_task, SCHED_NORMAL, 0);
+      			set_task_state(current_mp2_task->linux_task, TASK_UNINTERRUPTIBLE);
+			}
+			sel->state = RUNNING;
+
+			//For the preempting task
+			set_scheduler(sel->linux_task, SCHED_FIFO, 99);
+	      	current_mp2_task = sel;
+			wake_up_process(sel->linux_task);
+		}
+		mutex_unlock(&task_mutex);
+	}
+	printk("dispatcher exit.\n");
+	return 0;
+}
+
+void timer_handler(unsigned long task_pid){
+    mp2_task_struct *wakeup_task, *tmp, *n;
+	unsigned long flags;
+	spin_lock_irqsave(&mp2_lock, flags);
+	list_for_each_entry_safe(tmp, n, &task_list, list){
+		if(tmp->pid == task_pid){
+			wakeup_task = tmp;
+		}
+	}
+
+	if(!wakeup_task || wakeup_task->linux_task == NULL || wakeup_task->state != SLEEPING) {
+		spin_unlock_irqrestore(&mp2_lock, flags);
+		printk("timer of task %u failed with error.\n", task_pid);
+		return;
+	}
+	wakeup_task->state = READY;
+	spin_unlock_irqrestore(&mp2_lock, flags);
+	wake_up_process(dispatcher);
+	return;
+}
+
+void yielding(unsigned int pid){
+	unsigned long flags;
+    mp2_task_struct *sel, *tmp, *n;
+	spin_lock_irqsave(&mp2_lock, flags);
+    list_for_each_entry_safe(tmp, n, &task_list, list){
+		if(tmp->pid == pid){
+			sel = tmp;
+		}
+	}
+	spin_unlock_irqrestore(&mp2_lock, flags);
+
+	if(jiffies < sel->next_period){
+			sel->state = SLEEPING;
+			mod_timer(&(sel->wakeup_timer),sel->next_period);
+			mutex_lock_interruptible(&task_mutex);
+    		current_mp2_task = NULL;
+    		mutex_unlock(&task_mutex);
+            // wake up scheduler
+   			wake_up_process(dispatcher);
+            // sleep
+    		set_task_state(sel->linux_task, TASK_UNINTERRUPTIBLE);
+    		schedule();
+	}
+	sel->next_period += msecs_to_jiffies(sel->period);
+	printk("task %u finished!\n",pid);
+
+}
+
+int admission_control(unsigned int cpu_time, unsigned int period){
+	unsigned long flags;
+	mp2_task_struct *tmp;
+
+	spin_lock_irqsave(&mp2_lock,flags);
+	unsigned int ratio = (1000 * cpu_time) / period;
+	list_for_each_entry(tmp, &task_list, list)
+		ratio += (1000 * tmp->cpu_time) / tmp->period;
+	spin_unlock_irqrestore(&mp2_lock,flags);
+
+	return ratio < 693;
 }
 
 // mp1_init - Called when module is loaded
@@ -178,13 +293,9 @@ int __init mp2_init(void)
 
    mp2_cache = kmem_cache_create("mp2_task_struct",sizeof(mp2_task_struct), ARCH_MIN_TASKALIGN,SLAB_PANIC, NULL);
 
-   setup_timer(&my_timer, my_timer_callback, 0);
-   mod_timer(&my_timer, jiffies + msecs_to_jiffies(T_INTERVAL));
+   dispatcher = kthread_run(dispatch_thread, NULL, "dispatching thread");
 
-   my_wq = create_workqueue("my_queue");
    spin_lock_init(&mp2_lock);
-   my_work = (struct work_struct *)kmalloc(sizeof(struct work_struct), GFP_KERNEL);
-   INIT_WORK(my_work, my_work_function);
 
    printk(KERN_ALERT "MP2 MODULE LOADED\n");
    return 0;
@@ -199,18 +310,22 @@ void __exit mp2_exit(void){
    remove_proc_entry(FILE, proc_directory);
    remove_proc_entry(DIRECTORY, NULL);
 
+   int ret = kthread_stop(dispatcher);
+   if (ret != -EINTR)
+       printk("Counter thread has stopped\n");
+   mutex_destroy(&task_mutex);
+
+
    del_timer_sync(&my_timer);
-   process_list *tmp, *n;
-   list_for_each_entry_safe(tmp, n, &mp1_list, list) {
+   mp2_task_struct *tmp, *n;
+   list_for_each_entry_safe(tmp, n, &task_list, list) {
        list_del(&tmp->list);
-       kfree(tmp);
+       del_timer(&tmp->wakeup_timer);
+       kmem_cache_free(mp2_cache,pos);
    }
-   flush_workqueue(my_wq);
-   destroy_workqueue(my_wq);
 
    kmem_cache_destroy(mp2_cache);
-
-   kfree(my_work);
+   list_del(&task_list);
    printk(KERN_ALERT "MP2 MODULE UNLOADED\n");
 }
 
