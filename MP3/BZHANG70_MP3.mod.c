2039596484934d4e47bf87f638f55e8bc92c88f8
Zhang <bzhang70@sp18-cs423-013.cs.illinois.edu>
2018-04-18 16:36:03 -0500

    data

---
+++ MP3/BZHANG70_MP3.mod.c
@@ -0,0 +1,67 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xb6fd8534, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xd4cd1dfc, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0x54fe1637, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0xd2b09ce5, __VMLINUX_SYMBOL_STR(__kmalloc) },
+	{ 0x32902b91, __VMLINUX_SYMBOL_STR(cdev_init) },
+	{ 0xd6ee688f, __VMLINUX_SYMBOL_STR(vmalloc) },
+	{ 0x43a53735, __VMLINUX_SYMBOL_STR(__alloc_workqueue_key) },
+	{ 0xd2d1927b, __VMLINUX_SYMBOL_STR(hrtimer_forward) },
+	{ 0xe6bced05, __VMLINUX_SYMBOL_STR(find_vpid) },
+	{ 0xe418fde4, __VMLINUX_SYMBOL_STR(hrtimer_cancel) },
+	{ 0x1c98a306, __VMLINUX_SYMBOL_STR(remove_proc_entry) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0x999e8297, __VMLINUX_SYMBOL_STR(vfree) },
+	{ 0x91715312, __VMLINUX_SYMBOL_STR(sprintf) },
+	{ 0x7d11c268, __VMLINUX_SYMBOL_STR(jiffies) },
+	{ 0x4f8b5ddb, __VMLINUX_SYMBOL_STR(_copy_to_user) },
+	{ 0xd45ea32f, __VMLINUX_SYMBOL_STR(hrtimer_start_range_ns) },
+	{ 0x45898e55, __VMLINUX_SYMBOL_STR(proc_mkdir) },
+	{ 0x3744cf36, __VMLINUX_SYMBOL_STR(vmalloc_to_pfn) },
+	{ 0x1916e38c, __VMLINUX_SYMBOL_STR(_raw_spin_unlock_irqrestore) },
+	{ 0x37befc70, __VMLINUX_SYMBOL_STR(jiffies_to_msecs) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x20c55ae0, __VMLINUX_SYMBOL_STR(sscanf) },
+	{ 0x8c03d20c, __VMLINUX_SYMBOL_STR(destroy_workqueue) },
+	{ 0xf9892ee7, __VMLINUX_SYMBOL_STR(pid_task) },
+	{ 0x42160169, __VMLINUX_SYMBOL_STR(flush_workqueue) },
+	{ 0x879443b3, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
+	{ 0x2ea2c95c, __VMLINUX_SYMBOL_STR(__x86_indirect_thunk_rax) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+	{ 0xe9bcba71, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x680ec266, __VMLINUX_SYMBOL_STR(_raw_spin_lock_irqsave) },
+	{ 0xdbec9d3c, __VMLINUX_SYMBOL_STR(proc_create_data) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0x9f1dfe3c, __VMLINUX_SYMBOL_STR(remap_pfn_range) },
+	{ 0x83ba5fbb, __VMLINUX_SYMBOL_STR(hrtimer_init) },
+	{ 0x2e0d2f7f, __VMLINUX_SYMBOL_STR(queue_work_on) },
+	{ 0x4f6b400b, __VMLINUX_SYMBOL_STR(_copy_from_user) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "3BCAF4009F60336A5FCC440");
