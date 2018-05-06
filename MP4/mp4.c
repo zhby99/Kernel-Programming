b53b1d55282dbb0a48129500d77282a7bc6291e0
bzhang70 <bzhang70@illinois.edu>
2018-05-06 01:22:50 -0500

    mp4

--- MP4/mp4.c
+++ MP4/mp4.c
@@ -304,7 +304,6 @@ static int mp4_inode_permission(struct inode *inode, int mask)
 
 	 int ssid = ((struct mp4_security*)current_cred()->security)->mp4_flags;
 	 int osid = get_inode_sid(inode, dentry);
-	 dput(dentry);
 	 if (ssid == MP4_TARGET_SID && S_ISDIR(inode->i_mode)){
 		 return 0;
 	 }
